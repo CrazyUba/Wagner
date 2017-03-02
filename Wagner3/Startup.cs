415779dbff3e1a51b98448a35e12030564009f65
Ulrich Backes <backes@solthor.de>
2017-03-02 17:14:00 +0100

    w3 landing page started

---
+++ Wagner3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wagner3.Startup))]
+namespace Wagner3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Wagner3/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wagner3.MvcApplication" Language="C#" %>

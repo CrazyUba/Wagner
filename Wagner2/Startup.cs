fa48113d12c81304fe69ac5378ff7769bd815200
Ulrich Backes <backes@solthor.de>
2017-03-02 16:20:54 +0100

    w2 ready, w1 adjusted

---
+++ Wagner2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wagner2.Startup))]
+namespace Wagner2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
